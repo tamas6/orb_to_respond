3977352b3e9421c2c4448df54e026fdf0178110a
tamas6 <81620050+tamas6@users.noreply.github.com>
2024-02-05 17:52:00 +0100

    refactor script 🟢

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-var renderer, scene, camera, composer, circle, skelet, particle;
+
+let renderer, scene, camera, circle, skelet, particle;
 
 window.onload = function () {
 	init();
@@ -8,7 +9,7 @@ window.onload = function () {
 
 function init() {
 	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-	renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+	renderer.setPixelRatio(window.devicePixelRatio || 1);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.autoClear = false;
 	renderer.setClearColor(0x000000, 0.0);
@@ -29,24 +30,30 @@ function init() {
 	skelet = new THREE.Object3D();
 	particle = new THREE.Object3D();
 
-	scene.add(circle);
-	scene.add(skelet);
-	scene.add(particle);
+	scene.add(circle, skelet, particle);
 
-	var geometry = new THREE.TetrahedronGeometry(2, 0);
-	var geom = new THREE.IcosahedronGeometry(7, 1);
-	var geom2 = new THREE.IcosahedronGeometry(15, 1);
+	const geometry = new THREE.TetrahedronGeometry(2, 0);
+	const geom = new THREE.IcosahedronGeometry(7, 1);
+	const geom2 = new THREE.IcosahedronGeometry(15, 1);
 
-	var material = new THREE.MeshPhongMaterial({
+	const commonMaterial = {
 		flatShading: true,
 		uniforms: {
 			time: { value: 1.0 },
 			resolution: { value: new THREE.Vector2() },
 		},
+	};
+
+	const material = new THREE.MeshPhongMaterial({ ...commonMaterial });
+	const material2 = new THREE.MeshPhongMaterial({ ...commonMaterial });
+	const material3 = new THREE.MeshPhongMaterial({
+		color: 0xffffff,
+		wireframe: true,
+		side: THREE.DoubleSide,
 	});
 
-	for (var i = 0; i < 1000; i++) {
-		var mesh = new THREE.Mesh(geometry, material);
+	for (let i = 0; i < 1000; i++) {
+		const mesh = new THREE.Mesh(geometry, material);
 		mesh.position
 			.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
 			.normalize();
@@ -55,41 +62,28 @@ function init() {
 		particle.add(mesh);
 	}
 
-	var mat = new THREE.MeshPhongMaterial({
-		flatShading: true,
-		uniforms: {
-			time: { value: 1.0 },
-			resolution: { value: new THREE.Vector2() },
-		},
-	});
-
-	var mat2 = new THREE.MeshPhongMaterial({
-		color: 0xffffff,
-		wireframe: true,
-		side: THREE.DoubleSide,
-	});
-
-	var planet = new THREE.Mesh(geom, mat);
-	planet.scale.x = planet.scale.y = planet.scale.z = 16;
+	const planet = new THREE.Mesh(geom, material2);
+	planet.scale.set(16, 16, 16);
 	circle.add(planet);
 
-	var planet2 = new THREE.Mesh(geom2, mat2);
-	planet2.scale.x = planet2.scale.y = planet2.scale.z = 10;
+	const planet2 = new THREE.Mesh(geom2, material3);
+	planet2.scale.set(10, 10, 10);
 	skelet.add(planet2);
 
-	var ambientLight = new THREE.AmbientLight(0xf7f8fa);
+	const ambientLight = new THREE.AmbientLight(0xf7f8fa);
 	scene.add(ambientLight);
 
-	var lights = [];
-	lights[0] = new THREE.DirectionalLight(0xffffff, 1);
+	const lights = [
+		new THREE.DirectionalLight(0xffffff, 1),
+		new THREE.DirectionalLight(0x7b61ff, 1),
+		new THREE.DirectionalLight(0xf7f8fa, 1),
+	];
+
 	lights[0].position.set(1, 0, 0);
-	lights[1] = new THREE.DirectionalLight(0x7b61ff, 1);
 	lights[1].position.set(0.75, 1, 0.5);
-	lights[2] = new THREE.DirectionalLight(0xf7f8fa, 1);
 	lights[2].position.set(-0.75, -1, 0.5);
-	scene.add(lights[0]);
-	scene.add(lights[1]);
-	scene.add(lights[2]);
+
+	lights.forEach((light) => scene.add(light));
 
 	window.addEventListener("resize", onWindowResize, false);
 }
@@ -109,7 +103,7 @@ function animate() {
 	circle.rotation.y -= 0.003;
 	skelet.rotation.x -= 0.001;
 	skelet.rotation.y += 0.002;
-	renderer.clear();
 
+	renderer.clear();
 	renderer.render(scene, camera);
 }
